64c83e453eb57b3fe37c714a5676b756081f280e
AndrewKh7 <rjynhf125>
2020-07-20 15:56:57 +0300

    /hello and /goodbye work with prefinx /first. Create SecondController with /exit url.

--- src/main/java/ru/khrapkov/springMVCApp/beans/controllers/FirstController.java
+++ src/main/java/ru/khrapkov/springMVCApp/beans/controllers/FirstController.java
@@ -5,16 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/first") // все запросы должны будут начинаться с /first (например /first/hello)
 public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(){
-       return "first/hello"; // то есть файл лежит в папке first
+       return "first/hello"; // то есть файл лежит в проектк в папке views/first
     }
 
     @GetMapping("/goodbye")
     public String goodbyePage(){
-        return "first/goodbye";
+        return "first/goodbye"; // то есть файл лежит в проектк в папке views/first
     }
 
 }
