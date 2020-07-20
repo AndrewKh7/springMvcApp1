2895abaad1c1084536480c9e8cab5328d761d544
AndrewKh7 <rjynhf125>
2020-07-20 14:24:15 +0300

    Config project use java Configuration classes. delete xml files create MySpringMvcDispatcherServletInitializer class create SpringConfig class

---
+++ src/main/java/ru/khrapkov/springMVCApp/config/SpringConfig.java
@@ -0,0 +1,55 @@
+package ru.khrapkov.springMVCApp.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+@Configuration
+@ComponentScan("ru.khrapkov.springMVCApp.beans")
+@EnableWebMvc
+public class SpringConfig implements WebMvcConfigurer {
+    //реализуем этот интерфейс когда хотим настроить под себя spring mvc (метод configureViewResolvers) В данном случае
+    // в данном случае хтим использовать свой шаблонизатор Thymeleaf
+
+    private final ApplicationContext applicationContext;
+
+    @Autowired
+    public SpringConfig(ApplicationContext context) {
+        this.applicationContext = context;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(this.applicationContext);
+        templateResolver.setPrefix("/WEB-INF/views/");
+        templateResolver.setSuffix(".html");
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
+    //задает шаблонизатор, который мы зотим использоваьтся вмето стандартого
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        registry.viewResolver(resolver);
+    }
+
+}
+
