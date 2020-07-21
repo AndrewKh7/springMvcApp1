3ba0df836304418bb2897b25694923ccf6813d1e
AndrewKh7 <rjynhf125>
2020-07-21 16:55:15 +0300

    processing get request parameters

--- src/main/java/ru/khrapkov/springMVCApp/beans/controllers/FirstController.java
+++ src/main/java/ru/khrapkov/springMVCApp/beans/controllers/FirstController.java
@@ -3,19 +3,30 @@ package ru.khrapkov.springMVCApp.beans.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/first") // все запросы должны будут начинаться с /first (например /first/hello)
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
-       return "first/hello"; // то есть файл лежит в проектк в папке views/first
+    public String helloPage(HttpServletRequest request){
+        System.out.println("Hello");
+        System.out.println(request.getParameter("name"));
+        return "first/hello"; // то есть файл лежит в проекте в папке views/first
     }
 
     @GetMapping("/goodbye")
-    public String goodbyePage(){
+    public String goodbyePage(@RequestParam( required=false) String name,
+                              @RequestParam( required=false) String surname){
+        System.out.println(name);
+        System.out.println(surname);
         return "first/goodbye"; // то есть файл лежит в проектк в папке views/first
     }
 
 }
+
